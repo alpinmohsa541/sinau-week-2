9e3979e8d34da225c51a4de342cbae33c61fb7c5
alpinmohsa541 <alpinmohsa@gmail.com>
2024-11-03 18:22:01 +0700

    menyelesaikan soal no 3

--- soal3.js
+++ soal3.js
@@ -1,3 +1,12 @@
-function sumArray(){
-    
+function sumArray(numbers) {
+    if (numbers.length === 0) return 0;
+
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
 }
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(sumArray([]));               
