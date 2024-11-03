076a8b3b0f136ca6679ccd235fcf9f0c2e38333c
alpinmohsa541 <alpinmohsa@gmail.com>
2024-11-03 17:52:10 +0700

    menyelesaikan soal nomor 1

--- soal1.js
+++ soal1.js
@@ -1,3 +1,16 @@
-function pallidrome(){
-    
+function isPallidrome(str){
+    let j = str.length - 1
+    for(let i = 0; i< str.length / 2; i++){
+        if(str[i] != str[j]){
+            return false;
+        }
+        j--;
+    }
+    return true;
 }
+
+let str1 = "racecar";
+let str2 = "hello";
+
+console.log(isPallidrome(str1));
+console.log(isPallidrome(str2));
