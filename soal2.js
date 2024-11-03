cdfb886448e7573ddf1d64e02671cddf9b3f961d
alpinmohsa541 <alpinmohsa@gmail.com>
2024-11-03 18:06:34 +0700

    menyelesaikan soal no 2

--- soal2.js
+++ soal2.js
@@ -1,10 +1,17 @@
-function isFizz(){
-    for (let i = 1; i <= num; i++){
+function isFizzBuzz(Number){
+    for (let i = 1; i <= Number; i++){
         if (i % 15 === 0){
-            
             console.log("FizzBuzz");
-    }
-    else if (i % 3 === 0){
-        console.log("Fizz")
+        }
+        else if (i % 3 === 0){
+        console.log("Fizz");
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
     }
 }
+isFizzBuzz(100);
