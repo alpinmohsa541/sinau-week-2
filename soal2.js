66a5b3ab444fa3f4449408f3bb815f293c41c2b0
alpinmohsa541 <alpinmohsa@gmail.com>
2024-11-03 18:00:34 +0700

    menambahkan soal2.js

---
+++ soal2.js
@@ -0,0 +1,10 @@
+function isFizz(){
+    for (let i = 1; i <= num; i++){
+        if (i % 15 === 0){
+            
+            console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz")
+    }
+}
